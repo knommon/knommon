e95623f8d927d1c64190dc233f22eeee4bbe5d69
rrcobb <rwcobbjr@gmail.com>
2014-03-18 16:27:44 -0400

    small changes to layouts

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -1,16 +1,16 @@
 @section('header')
 	<div class="header">
 		<div class="pure-menu pure-menu-open pure-menu-horizontal">
-		    <a href="#" class="pure-menu-heading">
+		    <a href="/" class="pure-menu-heading">
 		    Knommon
 		    <!--this is where our logo will go, once we have it-->
 		    </a>
     		<ul>
-        		<li class="pure-menu-selected"><a href="#">Home</a></li>
-        		<li><a href="#">Projects</a></li>
-        		<li><a href="#">Resources</a></li>
-        		<li><a href="#">Skills</a></li>
-        		<li><a href="#">About</a></li>
+        		<li class="pure-menu-selected"><a href="index">Home</a></li>
+        		<li><a href="projects">Projects</a></li>
+        		<li><a href="resources">Resources</a></li>
+        		<li><a href="skills">Skills</a></li>
+        		<li><a href="about">About</a></li>
     		</ul>
 		</div>
 	</div>
