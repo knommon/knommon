f3e34a743e5ad499163645096e3b2730e2ceee9f
rrcobb <rwcobbjr@gmail.com>
2014-03-25 23:21:03 -0400

    taglines added to projects

---
+++ app/database/migrations/2014_03_26_024915_add_tagline.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTagline extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('projects', function(Blueprint $table)
+		{
+			$table->text('tagline')->after('title');	
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('projects', function(Blueprint $table)
+		{
+			$table->dropColumn('tagline');
+		});
+	}
+
+}
